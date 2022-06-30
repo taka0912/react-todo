e11825a71fd35c0755f14058b2c02baedc09681d
taka0912 <69418050+taka0912@users.noreply.github.com>
2022-06-30 23:14:49 +0900

    useStateの使用方法の勉強

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./styles.css";
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 export default function App() {
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>タスク追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク：0</div>
+    </>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoLost = ({ todos }) => {
+  return <div>{todos}</div>;
+};
+
+export default TodoLost;
