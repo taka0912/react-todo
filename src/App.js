baec9d566670f958c1b6f27398f29308b779a3b8
taka0912 <69418050+taka0912@users.noreply.github.com>
2022-07-04 00:06:58 +0900

    todoアプリ作成(チェックボックスのチェック付け外し実装)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 export default function App() {
-  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo1", completed: false }
+  ]);
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value;
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: "1", name: name, completed: false }];
+    });
+    todoNameRef.current.value = null;
+  };
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  };
+
   return (
     <>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>タスク追加</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input type="text" ref={todoNameRef} />
+      <button onClick={handleAddTodo}>タスク追加</button>
       <button>完了したタスクの削除</button>
       <div>残りのタスク：0</div>
     </>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoLost = ({ todos }) => {
-  return <div>{todos}</div>;
+const TodoLost = ({ todos, toggleTodo }) => {
+  return todos.map((todo) => (
+    <Todo todo={todo} key={todo.name} toggleTodo={toggleTodo} />
+  ));
 };
 
 export default TodoLost;
